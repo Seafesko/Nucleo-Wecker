49d43a193d88555b9078e55c244ed8d63670faff
braht <braht@DESKTOP-QCL936L.speedport.ip>
2020-12-14 13:33:04 +0100

    Basis Main Menu

--- es2020/src/main.c
+++ es2020/src/main.c
@@ -6,29 +6,29 @@
 #include <stm32l073xx.h>
 
 int main(void) {
+
+	//INIT
 	clock_setup_16MHz();
 	systick_setup();
 	i2c_setup();
 	lcd_init();
 	button_setup();
-	lcd_print_string("    Embedded    "
-	                 "    Systems!    ");
-
+	lcd_print_string("White --> Alarm!"
+	                 "Blue --> Setting");
+	//MAIN
 	while (1) {
-		lcd_print_string("    Embedded    "
-			             "    Systems!    ");
-		delay_ms(500);
-
 		if (read_button_0()) {
+			//call Alarm Clock subprocess
 			lcd_print_string("    Button 0    "
 						     "    Test!       ");
-		}else if(read_button_1() ) {
+		}else if(read_button_1()) {
+			//call settings subprocess
 			lcd_print_string("    Button 1    "
 							 "    Test!       ");
 		}else {
-			lcd_print_string("    Bla         "
-				             "    Test!       ");
+			lcd_print_string("White --> Alarm!"
+			                 "Blue --> Setting");
 		}
-		delay_ms(500);
+		delay_ms(50); //else Display refreshes too often
 	}
 }
