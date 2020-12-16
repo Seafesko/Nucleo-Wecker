ba362a11f98a800fca459a25016207362d34e533
braht <braht@DESKTOP-QCL936L.speedport.ip>
2020-12-16 18:46:45 +0100

    Strings for Textdisplay in Main

--- es2020/src/main.c
+++ es2020/src/main.c
@@ -3,6 +3,7 @@
 #include "button.h"
 #include "i2c.h"
 #include "lcd.h"
+#include "joystick.h"
 #include <stm32l073xx.h>
 
 int main(void) {
@@ -13,22 +14,31 @@ int main(void) {
 	i2c_setup();
 	lcd_init();
 	button_setup();
-	lcd_print_string("White --> Alarm!"
-	                 "Blue --> Setting");
+	//joystick_setup();
+
+
+	//Display Texts
+	const char* text_b0 = ("    Button 0    "
+		       	   	       "    Test!       ");
+	const char* text_b1 = ("    Button 1    "
+		       	   	       "    Test!       ");
+	const char* text_main = ("White --> Alarm!"
+            				 "Blue --> Setting");
+
+	//Display Main Menu Text
+	lcd_print_string(text_main);
+
 	//MAIN
 	while (1) {
 		if (read_button_0()) {
-			//call Alarm Clock subprocess
-			lcd_print_string("    Button 0    "
-						     "    Test!       ");
+			//TODO: call Alarm Clock subprocess here
+			lcd_print_string(text_b0);
 		}else if(read_button_1()) {
-			//call settings subprocess
-			lcd_print_string("    Button 1    "
-							 "    Test!       ");
+			//TODO: put all settings subprocess here
+			lcd_print_string(text_b1);
 		}else {
-			lcd_print_string("White --> Alarm!"
-			                 "Blue --> Setting");
+			lcd_print_string(text_main);
 		}
-		delay_ms(50); //else Display refreshes too often
+		delay_ms(75); //else Display refreshes too often
 	}
 }
