e5645b2500841cef2d9d79cd13235a008bfed928
braht <braht@DESKTOP-QCL936L.speedport.ip>
2020-12-30 18:39:23 +0100

    removing static declarations in header

--- es2020/include/settings.h
+++ es2020/include/settings.h
@@ -8,19 +8,8 @@ extern "C" {
 #include <stdint.h>
 #endif
 
-//internal functions
-static void add_diffi(void);
-static void sub_diffi(void);
-static void add_device(void);
-static void sub_device(void);
-static uint8_t add_setting(uint8_t setting_pos);
-static uint8_t sub_setting(uint8_t setting_pos);
-static void choose_diff(int8_t joy_d_y);
-static void choose_device(int8_t joy_d_y);
-static void test_touch(void);
-static void test_joy(void);
-
 /* external functions */
+
 uint8_t get_difficulty(void);
 uint8_t get_device(void);
 uint16_t settings(void);
